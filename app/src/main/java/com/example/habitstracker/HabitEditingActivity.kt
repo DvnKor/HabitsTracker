0192ba6bd57f27510a2cdbe220c852e50f841beb
etozhedankor <41688602+etozhedankor@users.noreply.github.com>
2020-03-11 22:38:41 +0500

    editing activity returns result

--- app/src/main/java/com/example/habitstracker/HabitEditingActivity.kt
+++ app/src/main/java/com/example/habitstracker/HabitEditingActivity.kt
@@ -1,7 +1,12 @@
 package com.example.habitstracker
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_habit_editing.*
 
 class HabitEditingActivity : AppCompatActivity() {
     private var name: String = ""
@@ -10,7 +15,7 @@ class HabitEditingActivity : AppCompatActivity() {
     private var numberOfRepeats: Int = 0
     private var numberOfDays: Int = 0
     private var color: Int = 0
-    private var priority: Int = 0
+    private var priority: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +25,41 @@ class HabitEditingActivity : AppCompatActivity() {
         numberOfRepeats = intent?.extras?.getInt("numberOfRepeats", 0) ?: 0
         numberOfDays = intent?.extras?.getInt("numberOfDays", 0) ?: 0
         color = intent?.extras?.getInt("color", 0) ?: 0
-        priority = intent?.extras?.getInt("priority", 0) ?: 0
+        priority = intent?.extras?.getString("priority", "") ?: ""
         setContentView(R.layout.activity_habit_editing)
+    }
+
+    private fun saveUserInput() {
+        name = editName.text.toString()
+        description = editDescription.text.toString()
+        type = findViewById<RadioButton>(typeRadioGroup.checkedRadioButtonId).text.toString()
+        numberOfRepeats = editNumberOfRepeats.text.toString().toInt()
+        numberOfDays = editNumberOfDays.text.toString().toInt()
+        priority = prioritySpinner.selectedItem.toString()
+        color = 0
+
+    }
+
+    private fun getUpdatedIntent(): Intent {
+        val updatedIntent = Intent()
+        updatedIntent.putExtra("name", name)
+        updatedIntent.putExtra("description", description)
+        updatedIntent.putExtra("type", type)
+        updatedIntent.putExtra("numberOfRepeats", numberOfRepeats)
+        updatedIntent.putExtra("numberOfDays", numberOfDays)
+        updatedIntent.putExtra("priority", priority)
+        updatedIntent.putExtra("color", color)
+        return updatedIntent
+    }
+
+    fun onSaveClick(view: View) {
+        saveUserInput()
+        setResult(Activity.RESULT_OK, getUpdatedIntent())
+        finish()
+    }
+
+    fun onCancelClick(view: View) {
+        setResult(Activity.RESULT_OK, getUpdatedIntent())
+        finish()
     }
 }
--- app/src/main/java/com/example/habitstracker/MainActivity.kt
+++ app/src/main/java/com/example/habitstracker/MainActivity.kt
@@ -1,23 +1,34 @@
 package com.example.habitstracker
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val changeHabitRequestCode = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fab = findViewById<View>(R.id.fab)
         fab.setOnClickListener(this::onFabClick)
     }
 
     private fun onFabClick(view: View) {
         val intent = Intent(this, HabitEditingActivity::class.java)
-        val result = startActivityForResult(intent, 0)
+        val result = startActivityForResult(intent, changeHabitRequestCode)
         Log.i("clicked", "clicked")
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != changeHabitRequestCode) return
+        if (resultCode == Activity.RESULT_OK) {
+            Log.i("deb", data?.extras?.getString("type") ?: "no")
+        }
+    }
+
 }
